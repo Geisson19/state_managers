5e36261d9fbb984d8f93c79857d5e6e3ab1a5bed
Geisson19 <gs.ponce20@uniandes.edu.co>
2022-07-20 19:10:11 -0500

    user model fixed

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,7 +1,15 @@
 class User {
   String name;
-  int? age;
-  List<String>? carrers;
+  int age;
+  List<String> carrers;
 
-  User({required this.name, this.age, this.carrers});
+  User({required this.name, required this.age, required this.carrers});
+
+  User copyWith({String? name, int? age, List<String>? carrers}) {
+    return User(
+      name: name ?? this.name,
+      age: age ?? this.age,
+      carrers: carrers ?? this.carrers,
+    );
+  }
 }
